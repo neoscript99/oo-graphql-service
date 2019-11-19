28ca346f257ea565609b3e380e583d1a0a6793e0
Neo <neoscript@gmail.com>
2019-11-19 21:45:05 +0800

    loginInfo加到UserStore，包括roles信息

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,5 +1,5 @@
 import gql from 'graphql-tag';
-import { sha256 } from 'js-sha256'
+import { sha256 } from 'js-sha256';
 import { message } from 'antd';
 import { Entity } from '../DomainStore';
 import { DomainService } from '../DomainService';
@@ -7,52 +7,53 @@ import { DomainGraphql } from '../DomainGraphql';
 import { UserStore } from '../stores';
 
 export interface UserEntity extends Entity {
-  account: string
-  name?: string
-  dept?: any
+  account: string;
+  name?: string;
+  dept?: any;
 }
 
 /**
- * 如果是系统自己认证：user 有 ， account 无
+ * 如果是系统自己认证：user 有 ， account 有
  * 如果接入了cas认证：account 有， user 不一定有
  */
 export interface LoginInfo {
-  success: boolean
-  token: string
-  user?: UserEntity
-  account: string
-  error?: string
+  success: boolean;
+  token: string;
+  user?: UserEntity;
+  account: string;
+  error?: string;
+  roles: string;
 }
 
 export interface CasConfig {
-  clientEnabled: boolean
-  casServerRoot?: string
-  defaultRoles?: string
+  clientEnabled: boolean;
+  casServerRoot?: string;
+  defaultRoles?: string;
 }
 
 export interface AfterLogin {
-  (loginInfo: LoginInfo): void
+  (loginInfo: LoginInfo): void;
 }
 
-const USERNAME_KEY = 'loginUsername'
-const PASSWORD_KEY = 'loginPassword'
+const USERNAME_KEY = 'loginUsername';
+const PASSWORD_KEY = 'loginPassword';
 
 export class UserService extends DomainService<UserStore> {
-
   constructor(private afterLogin: AfterLogin, domainGraphql: DomainGraphql) {
     super('user', UserStore, domainGraphql);
-    this.changeCurrentItem({})
+    this.changeCurrentItem({});
     //cas默认为true，初始化时去获取服务端的配置信息，如果为false，再显示登录界面
-    this.getCasConfig()
+    this.getCasConfig();
   }
 
-  login(username: string, password: string, remember: boolean = false): Promise<LoginInfo> {
-    return this.loginHash(username, sha256(password), remember)
+  login(username: string, password: string, remember = false): Promise<LoginInfo> {
+    return this.loginHash(username, sha256(password), remember);
   }
 
-  loginHash(username: string, passwordHash: string, remember: boolean = false): Promise<LoginInfo> {
-    return this.domainGraphql.apolloClient.mutate<{ login: LoginInfo }>({
-      mutation: gql`mutation loginAction {
+  loginHash(username: string, passwordHash: string, remember = false): Promise<LoginInfo> {
+    return this.domainGraphql.apolloClient
+      .mutate<{ login: LoginInfo }>({
+        mutation: gql`mutation loginAction {
                       login(username: "${username}", password: "${passwordHash}") {
                         success
                         error
@@ -60,121 +61,133 @@ export class UserService extends DomainService<UserStore> {
                         user{
                           id, account, name      
                         }
+                        roles
                       }
                     }`,
-      fetchPolicy: 'no-cache',
-      variables: {
-        ...this.domainGraphql.defaultVariables
-      }
-    })
+        fetchPolicy: 'no-cache',
+        variables: {
+          ...this.domainGraphql.defaultVariables,
+        },
+      })
       .then(data => {
-        const loginInfo = data.data!.login
+        const loginInfo = data.data!.login;
+        this.store.loginInfo = loginInfo;
         if (loginInfo.success) {
-          this.afterLogin(loginInfo)
-          this.changeCurrentItem(loginInfo.user as Entity)
-          if (remember)
-            this.saveLoginInfoLocal(username, passwordHash)
+          this.afterLogin(loginInfo);
+          this.changeCurrentItem(loginInfo.user as Entity);
+          if (remember) this.saveLoginInfoLocal(username, passwordHash);
         } else {
           message.info(loginInfo.error);
           this.clearLoginInfoLocal();
         }
-        return loginInfo
-      })
+        return loginInfo;
+      });
   }
 
   tryLocalLogin() {
-    const info = this.getLoginInfoLocal()
-    if (info.username && info.password)
-      this.loginHash(info.username, info.password)
+    const info = this.getLoginInfoLocal();
+    if (info.username && info.password) this.loginHash(info.username, info.password);
   }
 
   saveLoginInfoLocal(username: string, password: string) {
-    localStorage.setItem(USERNAME_KEY, username)
-    localStorage.setItem(PASSWORD_KEY, password)
+    localStorage.setItem(USERNAME_KEY, username);
+    localStorage.setItem(PASSWORD_KEY, password);
   }
 
   clearLoginInfoLocal() {
-    localStorage.removeItem(USERNAME_KEY)
-    localStorage.removeItem(PASSWORD_KEY)
+    localStorage.removeItem(USERNAME_KEY);
+    localStorage.removeItem(PASSWORD_KEY);
   }
 
   getLoginInfoLocal() {
     return {
       username: localStorage.getItem(USERNAME_KEY),
-      password: localStorage.getItem(PASSWORD_KEY)
-    }
+      password: localStorage.getItem(PASSWORD_KEY),
+    };
   }
 
   trySessionLogin(): Promise<LoginInfo> {
-    return this.domainGraphql.apolloClient.mutate<{ sessionLogin: LoginInfo }>({
-      mutation: gql`mutation sessionLoginAction {
-                      sessionLogin {
-                        success
-                        error
-                        token
-                        user{
-                          id, account, name      
-                        }
-                        account
-                      }
-                    }`,
-      fetchPolicy: 'no-cache',
-      variables: {
-        ...this.domainGraphql.defaultVariables
-      }
-    })
+    return this.domainGraphql.apolloClient
+      .mutate<{ sessionLogin: LoginInfo }>({
+        mutation: gql`
+          mutation sessionLoginAction {
+            sessionLogin {
+              success
+              error
+              token
+              user {
+                id
+                account
+                name
+              }
+              account
+              roles
+            }
+          }
+        `,
+        fetchPolicy: 'no-cache',
+        variables: {
+          ...this.domainGraphql.defaultVariables,
+        },
+      })
       .then(data => {
-        const loginInfo = data.data!.sessionLogin
+        const loginInfo = data.data!.sessionLogin;
+        this.store.loginInfo = loginInfo;
         if (loginInfo.success) {
-          loginInfo.user = loginInfo.user || { account: loginInfo.account }
-          this.afterLogin(loginInfo)
-          this.changeCurrentItem(loginInfo.user)
+          loginInfo.user = loginInfo.user || { account: loginInfo.account };
+          this.afterLogin(loginInfo);
+          this.changeCurrentItem(loginInfo.user);
         } else {
           console.debug(loginInfo.error);
         }
-        return loginInfo
-      })
+        return loginInfo;
+      });
   }
 
   sessionLogout() {
-    this.domainGraphql.apolloClient.mutate<{ sessionLogout: any }>({
-      mutation: gql`mutation sessionLogoutAction {
-                      sessionLogout {
-                        success
-                        error
-                      }
-                    }`,
-      fetchPolicy: 'no-cache',
-      variables: {
-        ...this.domainGraphql.defaultVariables
-      }
-    })
-      .then(data => data.data!.sessionLogout.success || console.warn(data.data!.sessionLogout.error))
+    this.domainGraphql.apolloClient
+      .mutate<{ sessionLogout: any }>({
+        mutation: gql`
+          mutation sessionLogoutAction {
+            sessionLogout {
+              success
+              error
+            }
+          }
+        `,
+        fetchPolicy: 'no-cache',
+        variables: {
+          ...this.domainGraphql.defaultVariables,
+        },
+      })
+      .then(data => data.data!.sessionLogout.success || console.warn(data.data!.sessionLogout.error));
   }
 
   getCasConfig(): Promise<CasConfig> {
-    return this.domainGraphql.apolloClient.query<{ getCasConfig: CasConfig }>({
-      query: gql`query getCasConfigQuery {
-                      getCasConfig {
-                        clientEnabled
-                        casServerRoot
-                        defaultRoles
-                      }
-                    }`,
-      fetchPolicy: 'no-cache',
-      variables: {
-        ...this.domainGraphql.defaultVariables
-      }
-    })
-      .then(data => {
-        this.store.casConfig = data.data!.getCasConfig
-        return data.data!.getCasConfig
+    return this.domainGraphql.apolloClient
+      .query<{ getCasConfig: CasConfig }>({
+        query: gql`
+          query getCasConfigQuery {
+            getCasConfig {
+              clientEnabled
+              casServerRoot
+              defaultRoles
+            }
+          }
+        `,
+        fetchPolicy: 'no-cache',
+        variables: {
+          ...this.domainGraphql.defaultVariables,
+        },
       })
+      .then(data => {
+        this.store.casConfig = data.data!.getCasConfig;
+        return data.data!.getCasConfig;
+      });
   }
 
   devLogin(account: string, token: string) {
-    this.changeCurrentItem({ account, token })
-    this.afterLogin({ user: { account }, account, token, success: true })
+    this.changeCurrentItem({ account, token });
+    this.afterLogin({ user: { account }, account, token, success: true, roles: 'Public' });
   }
-
 }
--- src/stores/UserStore.ts
+++ src/stores/UserStore.ts
@@ -1,11 +1,13 @@
 import { MobxDomainStore } from '../mobx';
 import { observable } from 'mobx';
-import { CasConfig } from '../services';
+import { CasConfig, LoginInfo } from '../services';
 
 export class UserStore extends MobxDomainStore {
   @observable
-  lastRoutePath: string = '/'
+  lastRoutePath = '/';
   @observable
-    //clientEnabled默认为true，不显示登录框，后台查询如果为false，再显示出来
-  casConfig: CasConfig = { clientEnabled: true }
+  //clientEnabled默认为true，不显示登录框，后台查询如果为false，再显示出来
+  casConfig: CasConfig = { clientEnabled: true };
+  @observable
+  loginInfo: LoginInfo | undefined;
 }
